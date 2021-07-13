39e8cb55e0d66fb074dd0424c97898867e3d5b11
Jesse Cox <jess.coxpdx@gmail.com>
2021-07-12 22:43:12 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,9 +7,7 @@ CONFIG_DIR="/etc/kbus_mqtt_client"
 CONFIG_FILE="kbus-daemon.cfg"
 
 INIT_DIR="/etc/init.d"
-INIT_SCRIPT="kbus_daemond"
-
-
+INIT_SCRIPT="kbus-daemond"
 
 #stop the runtime
 ./config_runtime runtime-version=0
@@ -27,4 +25,4 @@ mkdir $CONFIG_DIR && mv kbus_mqtt_client/$CONFIG_FILE $CONFIG_DIR
 mv init.d/$INIT_SCRIPT $INIT_DIR && chmod +x $INIT_DIR/$INIT_SCRIPT
 
 # make the linker script
-cd /etc/rc.d && ln -s ../init.d/$INIT_SCRIPT S99_kbus-daemond)
+cd /etc/rc.d && ln -s ../init.d/$INIT_SCRIPT S99_kbus-daemond
