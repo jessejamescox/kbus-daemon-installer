7cb7fb5987a4bb2ebb0822cc54a871478565f1e1
Jesse Cox <jess.coxpdx@gmail.com>
2021-07-13 14:17:05 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 JSONC_IPK="json-c_0.12.1+20160607_armhf.ipk"
 
-BINFILE="kbus-daemon"
+BIN_FILE="kbus-daemon"
 
 CONFIG_DIR="/etc/kbus_mqtt_client"
 CONFIG_FILE="kbus-daemon.cfg"
@@ -10,22 +10,29 @@ INIT_DIR="/etc/init.d"
 INIT_SCRIPT="kbus-daemond"
 
 #stop the runtime
+echo "switching off PLC runtime"
 ./config_runtime runtime-version=0
 
 # execute the ipk file
+echo "installing the json-c ipk"
 opkg install --force-reinstall ipk/$JSONC_IPK
 
 # copy the bin
-mv bin/$BINFILE /bin/ && chmod +x /bin/kbus-daemon
+echo "moving the binary file"
+mv bin/$BIN_FILE /bin/ && chmod +x /bin/$BIN_FILE
 
 #copy over the config
+echo "adding config directory and file"
 mkdir $CONFIG_DIR && mv kbus_mqtt_client/$CONFIG_FILE $CONFIG_DIR
 
 #copy the init.d script and link it to the start
+echo "adding init script and startup behavior"
 mv init.d/$INIT_SCRIPT $INIT_DIR && chmod +x $INIT_DIR/$INIT_SCRIPT
 
 # make the linker script
+echo "linking the startup to rc.d"
 cd /etc/rc.d && ln -s ../init.d/$INIT_SCRIPT S99_kbus-daemond
 
 # clean up 
+echo "cleaning up"
 rm -r /root/main.zip && rm -r /root/kbus-daemon-installer-main
